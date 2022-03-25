c32dba097c22ba1ba3dd718058da806c18cdfcd3
Jorge Antonio <moxruby00001@gmail.com>
2022-03-25 12:18:50 -0300

    Ponteiro atualizaçao

--- 9-25032022/ponteiro_struct.c
+++ 9-25032022/ponteiro_struct.c
@@ -4,7 +4,7 @@ seguintes dados: nome, idade, email
 */
 #include<stdio.h>
 #include<stdlib.h>
-void main()
+void main() {
     
     struct cliente{ //no fim do struck usar };
         char nome[30];
@@ -13,5 +13,17 @@ void main()
     };
 
     struct cliente novoCliente, *pNovoCliente;
+    pNovoCliente = &novoCliente;
+
+    printf("Entre com o nome do cliente:\n");
+    scanf("%s", &pNovoCliente->nome);
+
+    printf("Entre com o email do cliente:\n");
+    scanf("%s", &pNovoCliente->email);
+
+    printf("Entre com a idade do cliente:\n");
+    scanf("%d", &pNovoCliente->idade);
+
+    printf("o nome do cliente é %s\n",pNovoCliente->nome);
 
 }
