90a63e606e954f89df7ec68a1d1c11d89477a348
Jorge Antonio <moxruby00001@gmail.com>
2022-03-25 08:33:30 -0300

    Atividade atualizada

---
+++ 8-03032022/repeticao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main() {
+	float num, soma=0, media=0;
+	int i;
+	//inicio do laco for
+	for (i=1; i<=10; i++) {
+		//a partir deste ponto sao as instrucoes
+		//que devem ser executadas nas iteracoes
+		printf("Informe o numero:");
+		scanf("%f", &num);
+	}
+	//a media deve ser calculada apos a iteração
+	media = soma / 10;
+	printf("A media e: %f", media);
+}
