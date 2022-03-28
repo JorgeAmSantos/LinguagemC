ec68cb2c8121cfa4262cffef3658f741acc3d10a
Jorge Antonio <moxruby00001@gmail.com>
2022-03-28 12:03:01 -0300

    Elementos.h

--- 10-28032022/funcoes1.c
+++ 10-28032022/funcoes1.c
@@ -18,25 +18,25 @@ void separadorLinha(){
     }
     printf("\n");
 }
-void pularLInha(int qtdVezes){
+void pularLinha(int qtdVezes){
     for(int i = 1 ; i <= qtdVezes ; i++){
         printf("\n");
     }
 }
-void comparar(int v1, int v2){
+char* comparar(int v1, int v2){
     if(v1 > v2){
-        printf("O primeiro número é maior que o segundo número\n");
+        return"O primeiro número é maior que o segundo número\n";
     }
     else if(v1 == v2){
-        printf("O primeiro número é igual o segundo número\n");
+        return"O primeiro número é igual o segundo número\n";
     }
     else 
-        printf("O segundo número é maior que o primeiro numero\n");
+        return"O segundo número é maior que o primeiro numero\n";
 }
 
 void main(){
     int n1, n2;
-    system("clear"), //Limpar a tela do terminal
+    system("clear"); //Limpar a tela do terminal
     //Chamando a função separadorHT
     separadorHT();
     printf("\n\t\tPrograma que exibe uma mensagem\n ");
@@ -46,8 +46,9 @@ void main(){
     scanf("%d", &n1);
     printf("digite o segundo número ");
     scanf("%d", &n2);
-    pularLInha(2);
-    comparar(n1,n2);
+    pularLinha(2);
+    printf(comparar(n1,n2));
+    pularLinha(2);
     separadorLinha();
     separadorHT();
 }
