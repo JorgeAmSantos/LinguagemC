7096bba3f427dcd0720c85a61290f1f77d5c113a
Jorge Antonio <moxruby00001@gmail.com>
2022-03-31 12:02:22 -0300

    atualizaçao

--- 11-31032022/array2.c
+++ 11-31032022/array2.c
@@ -6,8 +6,9 @@ Programa que realiza a soma usando array
 #include"calculo.h"
 
 void main(){
-    int v[10] = {0,1,2,3,4,5,6,7,8,9};
+    int v[10] = {37,2,5,71,200,52,16,23,18,4};
     soma(v,10);
     maiorValor(v,10);
+    menorValor(v,10);
     
 }
--- 11-31032022/calculo.h
+++ 11-31032022/calculo.h
@@ -1,5 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
+/*
+Soma todos os valores do array
+*/
+
 void soma (int array[], int tamanho){
     int rs = 0;
     for(int i = 0; i < tamanho; i++){
@@ -7,6 +11,9 @@ void soma (int array[], int tamanho){
     }
     printf("%d\n",rs);
 }
+/*
+indica o maoir valor do array
+*/
 
 void maiorValor (int array[],int tamanho){
     int m = array[0];
@@ -16,5 +23,31 @@ void maiorValor (int array[],int tamanho){
         }
     }
     printf("O maior valor é %d\n", m);
+}
+ /*
+ indica o maior valor do array
+ */
 
+    void menorValor (int array[],int tamanho){
+    int m = array[0];
+    for(int i = 1; i > tamanho; i++){
+        if(array[i] < m){
+            m = array[i];
+        }
+    }
+    printf("O menor valor é %d\n", m);
+
+}
+
+void ordenar (int valores[], int tamanho){
+
+    for(int j = 1 ; j < tamanho ; j++){
+        int x = valores[j];
+        int i;
+        for(i = j-1 ; i>= 0 && valores[i] > x; i--){
+            valores[i + 1] = valores[i];
+        }
+        v[i+1] = x;
+    }
+    
 }
