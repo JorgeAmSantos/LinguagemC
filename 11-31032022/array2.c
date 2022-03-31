88193def5e44bc1a021c504c9c7cb9e0429ad57d
Jorge Antonio <moxruby00001@gmail.com>
2022-03-31 11:09:14 -0300

    Array

---
+++ 11-31032022/array2.c
@@ -0,0 +1,13 @@
+/*
+Programa que realiza a soma usando array
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include"calculo.h"
+
+void main(){
+    int v[10] = {0,1,2,3,4,5,6,7,8,9};
+    soma(v,10);
+    maiorValor(v,10);
+    
+}
